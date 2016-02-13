376a47b123ff5516195f01059ea07ce8f6526354
Albert <atschx@gmail.com>
2016-02-13 21:41:43 +0800

    设置1天用户记录失效

--- src/handler/collect.py
+++ src/handler/collect.py
@@ -27,10 +27,11 @@ class CiaCollectdHandler(tornado.web.RequestHandler):
             self.redis_conn.hset(uuid_str, "cdt", alexa['cdt'])
             self.redis_conn.hset(uuid_str, "url", alexa['url'])
             self.redis_conn.hset(uuid_str, "ref", alexa['ref'])
-            self.redis_conn.expire(uuid_str, 3600)
+            self.redis_conn.expire(uuid_str, 86400)
 
             # worker
             self.redis_conn.lpush("alexa_robot_worker", uuid_str)
+            self.redis_conn.expire(uuid_str, 86400)
 
             # print headers,body
         except Exception, e:
